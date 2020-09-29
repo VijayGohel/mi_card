acef1a96f91b0933f054bb96f727faac90f1efa7
vijay gohel <gohil5898@gmail.com>
2020-09-29 17:00:32 +0530

    lec3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(20.00, 50.00, 20.00, 0),
+            padding: EdgeInsets.all(20),
+            height: 100,
+            width: 100,
+            color: Colors.teal[100],
+            child: Text("Hello !"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
